2b2c87660dda7f4b263d147aae9cad1f8429ef97
Robert <rkohnen@uni-koblenz.de>
2016-11-30 19:19:26 +0100

    Added Accuracy score

--- Assignment04.py
+++ Assignment04.py
@@ -3,13 +3,13 @@ import pandas as pd
 import numpy as np
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 df = pd.read_csv('data.csv')
 #TODO: What is the first column? Maybe names or sth?
 #TODO: The very first originates from pandas...
 df.replace(' ?', np.nan, inplace=True)
 df = df.dropna()
-
 
 for i in df:
     df[i] = df[i].map(lambda x: x.lstrip(' ').rstrip(' ') if isinstance(x, str) else x)
@@ -21,13 +21,21 @@ df.replace('S', 0, inplace = True)
 df.replace('C', 1, inplace = True)
 df.replace('Q', 2, inplace = True)
 
-
 #print(df.head(50))
-
 
 features = list(df.columns[2:9])
 #print(features)
 y = df["survived"]
 X = df[features]
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1 )
+
 dt = DecisionTreeClassifier(min_samples_split=20, random_state=99)
-dt.fit(X, y)
+dt.fit(x_train, y_train)
+
+res = dt.predict(x_test)
+#TODO Bugfixing?
+#TODO Accuracy is not 1 when you do print(accuracy_score(y_train, dt.predict(x_train)))
+print("Decision Tree Classifier")
+print(accuracy_score(y_test, res))
+
